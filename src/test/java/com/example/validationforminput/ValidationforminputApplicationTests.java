7fafe84449297e57139a91c3a2df5300b74d02fd
shantel-dot <taylorteep2@yahoo.com>
2021-02-08 20:27:22 -0600

    Initial commit

---
+++ src/test/java/com/example/validationforminput/ValidationforminputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validationforminput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationforminputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
